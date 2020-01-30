b601ef41efb02aabd206b4a2b465f3fe587d459d
SIonut0122 <sionut0122@yahoo.com>
2020-01-30 14:18:52 +0100

    Updates

---
+++ precache-manifest.c7d8dd97df1ed3c751bb4956dadd4468.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391a5b52d01957d294a5ff02040194f0",
+    "url": "/musicplayer/index.html"
+  },
+  {
+    "revision": "f8f5f58abf3641045ddc",
+    "url": "/musicplayer/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "54bd939c719413496ab5",
+    "url": "/musicplayer/static/css/main.9ff1cda9.chunk.css"
+  },
+  {
+    "revision": "f8f5f58abf3641045ddc",
+    "url": "/musicplayer/static/js/2.34d55260.chunk.js"
+  },
+  {
+    "revision": "19cb1612fb420b3e42a6d87b889177e7",
+    "url": "/musicplayer/static/js/2.34d55260.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54bd939c719413496ab5",
+    "url": "/musicplayer/static/js/main.0c89ddb2.chunk.js"
+  },
+  {
+    "revision": "f2924a26b5426e1c9661",
+    "url": "/musicplayer/static/js/runtime-main.6004d8a3.js"
+  },
+  {
+    "revision": "419dd7c7f8a0fd0c62f95114d0830a03",
+    "url": "/musicplayer/static/media/1.419dd7c7.png"
+  },
+  {
+    "revision": "746f757a128285c38c435735f6440776",
+    "url": "/musicplayer/static/media/Alan_Walker_-_Faded.746f757a.mp3"
+  },
+  {
+    "revision": "f6f797731473d92b8db6a0c13ff2b99e",
+    "url": "/musicplayer/static/media/Avicii_-_Waiting_for_love.f6f79773.mp3"
+  },
+  {
+    "revision": "4bc3172c79a8b8bf534ab8f345e6a619",
+    "url": "/musicplayer/static/media/CalvinHarris_This_Is_What_You_Came_For_ft_Rihanna.4bc3172c.mp3"
+  },
+  {
+    "revision": "ad1ddbc8ee781bd86d39c1b8431ea2e1",
+    "url": "/musicplayer/static/media/Dua_Lipa_-_Dont_Start_Now.ad1ddbc8.mp3"
+  },
+  {
+    "revision": "af954410b3665780a76c9bc810539128",
+    "url": "/musicplayer/static/media/Duke_Dumont_feat_A_M_E-Need_U_100.af954410.mp3"
+  },
+  {
+    "revision": "aa9a371926d19cfb954589938ad587a1",
+    "url": "/musicplayer/static/media/Ed_Sheeran_-_I_See_Fire.aa9a3719.mp3"
+  },
+  {
+    "revision": "11c89f61a10dfabd10f416d105c7e90c",
+    "url": "/musicplayer/static/media/Flo_Rida_-_Wild_Ones_ft_Sia.11c89f61.mp3"
+  },
+  {
+    "revision": "e92372d22993aef40545a824011b4484",
+    "url": "/musicplayer/static/media/Imagine_Dragons_-_Believer.e92372d2.mp3"
+  },
+  {
+    "revision": "83902aef9ee55f9d0b48ccae14989bcc",
+    "url": "/musicplayer/static/media/Jay_Sean_ft_Gucci_Mane_-_With_you.83902aef.mp3"
+  },
+  {
+    "revision": "e9a4717973baa956f93018506c0aef39",
+    "url": "/musicplayer/static/media/Jessie_J_-_Do_It_Like_A_Dude.e9a47179.mp3"
+  },
+  {
+    "revision": "e9e78c83d3d7442f231ba8f981a82ba2",
+    "url": "/musicplayer/static/media/Lost_Frequencies_ft_James Blunt_-_Melody.e9e78c83.mp3"
+  },
+  {
+    "revision": "2dfab37f55acdadba6f8b6388f163aa3",
+    "url": "/musicplayer/static/media/Lost_Frequencies_ft_James_Blunt.2dfab37f.jpg"
+  },
+  {
+    "revision": "bd72448300c38ed66b2ce4f16f5cb06a",
+    "url": "/musicplayer/static/media/Lukas_Graham_-_Mama_Said.bd724483.mp3"
+  },
+  {
+    "revision": "9896ed47e4f69adf4dcb196e3a0f313e",
+    "url": "/musicplayer/static/media/Passenger_Let_Her_Go.9896ed47.mp3"
+  },
+  {
+    "revision": "cc67003a796beeda57d57d57ac84e549",
+    "url": "/musicplayer/static/media/Robin_Schulz_–_OK_feat_James_Blunt.cc67003a.mp3"
+  },
+  {
+    "revision": "aa28e56982fac3c7f9c925e180016308",
+    "url": "/musicplayer/static/media/Skrillex&Diplo_ft_Kai.aa28e569.mp3"
+  },
+  {
+    "revision": "edefbab804ab768b0de8355b8755c42a",
+    "url": "/musicplayer/static/media/alan_walker.edefbab8.jpg"
+  },
+  {
+    "revision": "85f9c1624957f06db261b483e1ce4d2f",
+    "url": "/musicplayer/static/media/avicii.85f9c162.jpeg"
+  },
+  {
+    "revision": "7aabe51fa3f19d7b9d255efbbbcef36a",
+    "url": "/musicplayer/static/media/bg-min.7aabe51f.png"
+  },
+  {
+    "revision": "b67994ac4782a29cbfe0a3e19877809c",
+    "url": "/musicplayer/static/media/calvin_harris_feat_rihanna.b67994ac.png"
+  },
+  {
+    "revision": "3e385a6cd509925cb405689266eb5c31",
+    "url": "/musicplayer/static/media/dua_lipa.3e385a6c.jpg"
+  },
+  {
+    "revision": "5894bafb5367356c3435f32c74cfa786",
+    "url": "/musicplayer/static/media/duke_dumont.5894bafb.jpg"
+  },
+  {
+    "revision": "03fc4773edec5ec527accc2d3c04d53c",
+    "url": "/musicplayer/static/media/ed_sheeran.03fc4773.jpg"
+  },
+  {
+    "revision": "43c9669d911a759a6c7cc30a7df27b19",
+    "url": "/musicplayer/static/media/flo_rida.43c9669d.jpg"
+  },
+  {
+    "revision": "aac4a3e662552c964ba54766ba4324ba",
+    "url": "/musicplayer/static/media/imagine_dragons.aac4a3e6.jpg"
+  },
+  {
+    "revision": "c73c2756eb2d3236a982244135aacac0",
+    "url": "/musicplayer/static/media/jay_sean.c73c2756.jpg"
+  },
+  {
+    "revision": "0441857b4c8203f84fb981df62e49fb0",
+    "url": "/musicplayer/static/media/jessie_j.0441857b.jpg"
+  },
+  {
+    "revision": "4ee40f7063de0637ab9255b199cf679d",
+    "url": "/musicplayer/static/media/loading_headphones.4ee40f70.png"
+  },
+  {
+    "revision": "b7f9d2423082570dd7b6142385b6679d",
+    "url": "/musicplayer/static/media/loading_headphones_2.b7f9d242.png"
+  },
+  {
+    "revision": "9915f39ef3b1ff61eba72c015f9af71c",
+    "url": "/musicplayer/static/media/lukas_graham.9915f39e.jpg"
+  },
+  {
+    "revision": "137712e1c4e634c64b4455c965cc9548",
+    "url": "/musicplayer/static/media/passenger.137712e1.jpg"
+  },
+  {
+    "revision": "11a27b0b112ab42700a24c778b117452",
+    "url": "/musicplayer/static/media/skrillex_diplo.11a27b0b.jpeg"
+  }
+]);
